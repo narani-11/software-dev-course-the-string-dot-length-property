a25ea42582136a5599cf6dd279e942b376720a64
narani-11 <naranitejaswini29@gmail.com>
2025-07-08 23:50:07 -0500

    Update the-string-dot-length-property.js

--- the-string-dot-length-property.js
+++ the-string-dot-length-property.js
@@ -26,5 +26,29 @@ let lengthThree; // Length of stringThree
 
 // Your code here
 
+// Predefined strings
+let stringOne = "Coding Bootcamp";
+let stringTwo = "JavaScript";
+let stringThree = "";
+
+// Assign the length of each string to the corresponding variable
+let lengthOne = stringOne.length;   // 15
+let lengthTwo = stringTwo.length;   // 10
+let lengthThree = stringThree.length; // 0
+
+// results to verify
+console.log(lengthOne);   // 15
+console.log(lengthTwo);   // 10
+console.log(lengthThree); // 0
+
+
+
+
+
+
+
+
+
+
 
 
