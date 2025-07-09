af7e3fbfe2fa3d5deaf368202a9dd5be2147df65
narani-11 <naranitejaswini29@gmail.com>
2025-07-09 16:28:51 -0500

    Update the-string-dot-length-property.js

--- the-string-dot-length-property.js
+++ the-string-dot-length-property.js
@@ -26,5 +26,24 @@ let lengthThree; // Length of stringThree
 
 // Your code here
 
+// Predefined strings
+let stringOne = "Coding Bootcamp";
+let stringTwo = "JavaScript";
+let stringThree = "";
+
+//  task: Assign the length of each string to the corresponding variable
+let lengthOne = stringOne.length;   // Length of stringOne
+let lengthTwo = stringTwo.length;   // Length of stringTwo
+let lengthThree = stringThree.length; // Length of stringThree
+
+// Log the results to check
+console.log(lengthOne);   // 15
+console.log(lengthTwo);   // 10
+console.log(lengthThree); // 0
+
+Explanation:
+•	.length counts the number of characters in the string.
+•	It works even if the string is empty (returns 0).
+
 
 
